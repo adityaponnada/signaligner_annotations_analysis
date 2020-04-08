151414ce4136fa44e8ba0734b7e6597d13c47b05
adityaponnada <ponnada.a@husky.neu.edu>
2020-04-07 23:07:43 -0500

    Read in playlog as a dataframe of string list

--- compute_usage_logs.R
+++ compute_usage_logs.R
@@ -1,3 +1,15 @@
 ## Import libraries
 library(rjson)
-library(jsonLite)
+library(rlist)
+
+
+## Read in the play log file
+playlog_path = "D:/Signaligner_Test_Datasets/Expert_labels/"
+play_log_filename = "Exp1_labels/playlog"
+
+## test read as a df
+log_file <- read.fwf(file=paste0(playlog_path, play_log_filename), widths = 1000)
+names(log_file) <- "LOG_JSON"
+log_file$UPDATED_JSON <- paste("[", log_file$LOG_JSON, "]")
+
+
