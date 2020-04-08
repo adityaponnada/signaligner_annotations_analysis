7d1f0b05b43b245f0f6c280144ad4c5964576c03
adityaponnada <ponnada.a@husky.neu.edu>
2020-04-07 23:32:17 -0500

    Convert the data frame row into JSON object list

--- compute_usage_logs.R
+++ compute_usage_logs.R
@@ -1,6 +1,7 @@
 ## Import libraries
 library(rjson)
 library(rlist)
+library(jsonlite)
 
 
 ## Read in the play log file
@@ -10,6 +11,8 @@ play_log_filename = "Exp1_labels/playlog"
 ## test read as a df
 log_file <- read.fwf(file=paste0(playlog_path, play_log_filename), widths = 1000)
 names(log_file) <- "LOG_JSON"
-log_file$UPDATED_JSON <- paste("[", log_file$LOG_JSON, "]")
+log_file$UPDATED_JSON <- paste0("[", log_file$LOG_JSON, "]")
 
+log_file$UPDATED_JSON <- lapply(log_file$LOG_JSON, fromJSON)
 
+res <- jsonlite::stream_in(textConnection(as.character(log_file$LOG_JSON)))
