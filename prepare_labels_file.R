0fc4625f2f181e17980942fd552fc02c6b0bb04f
adityaponnada <ponnada.a@husky.neu.edu>
2020-04-09 02:17:23 -0500

    Read seven day data instead

--- prepare_labels_file.R
+++ prepare_labels_file.R
@@ -8,9 +8,9 @@ library(plyr)
 ## Import labels file
 user_label_path = "D:/Signaligner_Test_Datasets/Expert_labels/"
 
-folder_name = "Exp2_labels/"
+folder_name = "Exp1_labels/"
 
-label_file_name = "StephenRightWristWearNonwearSleep3.csv"
+label_file_name = "LeftWrist_TAS1E23150023_2019_06_27_RAW.csv"
 
 user_labels <- read.csv(file = paste0(user_label_path, folder_name, label_file_name), header = TRUE, sep = ",")
 
@@ -20,5 +20,5 @@ user_only_labels <- subset(user_labels, user_labels$SOURCE == "Player")
 algo_only_labels <- subset(user_labels, user_labels$SOURCE == "Algo")
 
 ## Save the files
-write.csv(user_only_labels, file = paste0(user_label_path, folder_name, "Exp2_only_labels.csv"), row.names = FALSE, sep = ",")
-write.csv(algo_only_labels, file = paste0(user_label_path, folder_name, "Algo2_only_labels.csv"), row.names = FALSE, sep = ",")
+write.csv(user_only_labels, file = paste0(user_label_path, folder_name, "Exp1_only_labels.csv"), row.names = FALSE, sep = ",")
+write.csv(algo_only_labels, file = paste0(user_label_path, folder_name, "Algo1_only_labels.csv"), row.names = FALSE, sep = ",")
