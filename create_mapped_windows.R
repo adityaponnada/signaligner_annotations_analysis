0c7ee8d4d367cd9e886554acd7d8a8cfa03245e5
adityaponnada <ponnada.a@husky.neu.edu>
2020-04-09 21:27:51 -0500

    Create feature mapped label files for 30 second windows

--- create_mapped_windows.R
+++ create_mapped_windows.R
@@ -1,3 +1,53 @@
 ## Include libraries
 
 library(reshape2)
+options(digits.secs = 3)
+
+## Read the matched labels file
+
+matched_file <- read.csv(file="D:/Signaligner_Test_Datasets/Expert_labels/data_label_master.csv", header = TRUE, sep = ",")
+
+## Filter out the agreed labels
+agreed_labels <- subset(matched_file, matched_file$EXP_1_LABELS == matched_file$EXP_2_LABELS)
+
+agreed_labels <- na.omit(agreed_labels)
+
+## Read the combined feature file
+
+feature_file <- read.csv(file="D:/Signaligner_Test_Datasets/MISC/IEEEvis_Datasets/STEPHEN_WRIST_DATA/combined_feature_file.csv", header = TRUE, sep = ",")
+
+## Convert to date time objects
+agreed_labels$TIME_STAMP <- as.POSIXct(agreed_labels$TIME_STAMP, format="%Y-%m-%d %H:%M:%OS")
+
+feature_file$START_TIME <- as.POSIXct(feature_file$START_TIME, format="%Y-%m-%d %H:%M:%OS")
+feature_file$STOP_TIME <- as.POSIXct(feature_file$STOP_TIME, format="%Y-%m-%d %H:%M:%OS")
+feature_file$HEADER_TIME_STAMP <- as.POSIXct(feature_file$HEADER_TIME_STAMP, format="%Y-%m-%d %H:%M:%OS")
+
+
+### Add mapped labels
+feature_file$LABEL_CONSENSUS <- NA
+feature_file$LABEL_INCL_TRANSITIONS <- NA
+
+for (i in 1:nrow(feature_file)){
+  temp_subset <- subset(agreed_labels, agreed_labels$TIME_STAMP >= feature_file$START_TIME[i] & agreed_labels$TIME_STAMP < feature_file$STOP_TIME[i])
+  # feature_file$LABEL_CONSENSUS[i] <- as.character(tail(names(sort(table(temp_subset$EXP_1_LABELS))), 1))
+  temp_subset$EXP_1_LABELS <- factor(temp_subset$EXP_1_LABELS)
+  nlvl <- nlevels(temp_subset$EXP_1_LABELS)
+  if (nrow(temp_subset) > 0){
+    test_table <- table(temp_subset$EXP_1_LABELS)
+    feature_file$LABEL_CONSENSUS[i] <- names(test_table)[test_table==max(test_table)]
+    if (nlvl>1){
+      feature_file$LABEL_INCL_TRANSITIONS[i] <- "Transition"
+    } else {
+      feature_file$LABEL_INCL_TRANSITIONS[i] <- feature_file$LABEL_CONSENSUS[i]
+    }
+     
+  }
+  else {
+    feature_file$LABEL_CONSENSUS[i] <- "Unknown"
+    feature_file$LABEL_INCL_TRANSITIONS[i] <- "Unkown"
+  }
+  
+}
+
+write.csv(feature_file, file = "D:/Signaligner_Test_Datasets/MISC/IEEEvis_Datasets/STEPHEN_WRIST_DATA/labeled_windows_file.csv", row.names = FALSE, sep = ",")
