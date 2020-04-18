6abdeb76c3591c08126a5f1a1b4297f18ff8e058
adityaponnada <ponnada.a@husky.neu.edu>
2020-04-17 23:24:54 -0500

    Read the label latest json file

--- convert_label_jason_tocsv.R
+++ convert_label_jason_tocsv.R
@@ -1,2 +1,6 @@
 # Import libraries
 library(rjson)
+
+## read the json file
+label_json <- fromJSON(file="C:/Users/Aditya/Documents/SignalignerData/labelsets/StephenRightWristWearNonwearSleep3/DEFAULT/labels.latest.json")
+
