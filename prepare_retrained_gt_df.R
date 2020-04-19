464456e67bfa6df1fd81b4d0d5b5fb6dcc57e118
adityaponnada <ponnada.a@husky.neu.edu>
2020-04-19 13:35:53 -0500

    Create a combined labels data frame to compare predictions

--- prepare_retrained_gt_df.R
+++ prepare_retrained_gt_df.R
@@ -10,6 +10,11 @@ retrained_swan_df <- read.csv(file="D:/Signaligner_Test_Datasets/MISC/IEEEvis_Da
 
 feature_filter_unknown_df <- subset(feature_file, feature_file$LABEL_CONSENSUS != "Unknown")
 
-retrained_swan_df$NEW_SWAN[retrained_swan_df$PREDICTED_SMOOTH == 0] <- "Sleep"
-retrained_swan_df$NEW_SWAN[retrained_swan_df$PREDICTED_SMOOTH == 1] <- "Wear"
+retrained_swan_df$NEW_SWAN[retrained_swan_df$PREDICTED_SMOOTH == 0] <- "Wear"
+retrained_swan_df$NEW_SWAN[retrained_swan_df$PREDICTED_SMOOTH == 1] <- "Sleep"
 retrained_swan_df$NEW_SWAN[retrained_swan_df$PREDICTED_SMOOTH == 2] <- "Nonwear"
+
+labels_combined_df <- feature_filter_unknown_df[, c("LABEL_CONSENSUS", "EXP_1_LABELS", "EXP_2_LABELS", "GROUND_TRUTH", "OLD_SWAN")]
+
+labels_combined_df$NEW_SWAN <- retrained_swan_df$NEW_SWAN
+
