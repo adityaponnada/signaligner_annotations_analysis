b7c18f12189a0124f6ed73ed80504c0aa56256fe
adityaponnada <ponnada.a@husky.neu.edu>
2020-04-19 13:11:37 -0500

    Convert predictions to categories

--- prepare_retrained_gt_df.R
+++ prepare_retrained_gt_df.R
@@ -10,3 +10,6 @@ retrained_swan_df <- read.csv(file="D:/Signaligner_Test_Datasets/MISC/IEEEvis_Da
 
 feature_filter_unknown_df <- subset(feature_file, feature_file$LABEL_CONSENSUS != "Unknown")
 
+retrained_swan_df$NEW_SWAN[retrained_swan_df$PREDICTED_SMOOTH == 0] <- "Sleep"
+retrained_swan_df$NEW_SWAN[retrained_swan_df$PREDICTED_SMOOTH == 1] <- "Wear"
+retrained_swan_df$NEW_SWAN[retrained_swan_df$PREDICTED_SMOOTH == 2] <- "Nonwear"
